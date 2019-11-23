544ad6e3c0ab73fab65db6b1ca6548f5371e2b91
Peter Nelson <peter@peterdn.com>
2019-11-23 16:36:56 +0000

    Remove find_path function and tests

--- npc.py
+++ npc.py
@@ -12,34 +12,6 @@ class NPC(ABC):
         self.y = 0
 
 
-def find_path(fromx, fromy, tox, toy, impassable=None):
-    if impassable is None:
-        impassable = ''
-
-    if fromx == tox:
-        delta = int(sign(toy - fromy))
-        return [(fromx, y) for y in range(fromy, toy + delta, delta)]
-
-    gradient = (toy - fromy) / (tox - fromx)
-    print(f"gradient = {gradient}")
-
-    path = []
-    x, y = fromx, fromy
-    if abs(gradient) > 1:
-        while y != toy:
-            path.append((round(x), y))
-            y += int(sign(toy - fromy))
-            x += sign(toy - fromy) * ((tox - fromx) / (toy - fromy))
-    else:
-        while x != tox:
-            path.append((x, round(y)))
-            x += int(sign(tox - fromx))
-            y += sign(tox - fromx) * gradient
-
-    path.append((tox, toy))
-    return path
-
-
 def successors(map, x, y, impassable=None):
     if impassable is None:
         impassable = ''
