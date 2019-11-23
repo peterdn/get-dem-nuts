851c1f341ef928125aa351f3b4b2d563f998fc65
Peter Nelson <peter@peterdn.com>
2019-11-23 13:25:07 +0000

    Initial implementation of A* pathfinding, tests

--- npc.py
+++ npc.py
@@ -1,5 +1,6 @@
 import functools
 import math
+from queue import PriorityQueue
 
 sign = functools.partial(math.copysign, 1)
 
@@ -35,3 +36,75 @@ def find_path(fromx, fromy, tox, toy, impassable=None):
 
     path.append((tox, toy))
     return path
+
+
+def successors(map, x, y, impassable=None):
+    if impassable is None:
+        impassable = ''
+
+    height = len(map)
+    width = len(map[0])
+
+    def valid_successor(p):
+        (px, py) = p
+        if px == x and py == y:
+            return False
+        if px < 0 or py < 0 or px >= width or py >= height:
+            return False
+        return map[py][px] not in impassable
+
+    successors = [(sx, sy) for sx in range(x-1, x+2)
+                  for sy in range(y-1, y+2)]
+
+    successors = list(filter(valid_successor, successors))
+    return successors
+
+
+def visit(visited, x, y, parentx=None, parenty=None):
+    visited[y][x] = {}
+    visited[y][x]['cost'] = 0
+    visited[y][x]['parent'] = (parentx, parenty)
+    if parentx is not None and parenty is not None:
+        visited[y][x]['cost'] = visited[parenty][parentx]['cost'] + 1
+
+
+def dist(x, y, tx, ty):
+    return math.sqrt((x - tx)**2 + (y - ty)**2)
+
+
+def reconstruct_path(visited, fromx, fromy, tox, toy):
+    (cx, cy) = (tox, toy)
+    path = [(cx, cy)]
+    while (cx, cy) != (fromx, fromy):
+        (cx, cy) = visited[cy][cx]['parent']
+        path.append((cx, cy))
+    path.reverse()
+    return path
+
+
+def find_path_astar(map, fromx, fromy, tox, toy, impassable=None):
+    if impassable is None:
+        impassable = ''
+
+    height = len(map)
+    width = len(map[0])
+
+    visited = [[False for x in range(width)] for y in range(height)]
+    visit(visited, fromx, fromy)
+    fringe = PriorityQueue()
+    fringe.put((0, (fromx, fromy)))
+    while not fringe.empty():
+        (p, (cx, cy)) = fringe.get()
+        if (cx, cy) == (tox, toy):
+            break
+        print(f"Popped {(cx, cy)}")
+        ss = successors(map, cx, cy, impassable)
+        for (sx, sy) in ss:
+            if not visited[sy][sx]:
+                visit(visited, sx, sy, cx, cy)
+                hcost = visited[sy][sx]['cost'] + dist(sx, sy, tox, toy)
+                fringe.put((hcost, (sx, sy)))
+    if (cx, cy) == (tox, toy):
+        return reconstruct_path(visited, fromx, fromy, tox, toy)
+    else:
+        return None
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from npc import find_path
+from npc import find_path, find_path_astar, successors
 
 
 class TestNPC:
@@ -73,3 +73,83 @@ class TestNPC:
         path = find_path(fromx, fromy, tox, toy)
         assert path == [(13, 13), (12, 12), (11, 11), (11, 10), (10, 9),
                         (9, 8), (8, 7), (8, 6), (7, 5), (6, 4)]
+
+    def test_successors(self):
+        map = [['x' for x in range(10)] for y in range(10)]
+        ss = successors(map, 4, 5)
+        assert ss == [(3, 4), (3, 5), (3, 6), (4, 4),
+                      (4, 6), (5, 4), (5, 5), (5, 6)]
+
+        ss = successors(map, 0, 0)
+        assert ss == [(0, 1), (1, 0), (1, 1)]
+
+        ss = successors(map, 9, 9)
+        assert ss == [(8, 8), (8, 9), (9, 8)]
+
+    def test_successors_impassable(self):
+        map = [['x' for x in range(10)] for y in range(10)]
+        map[6][3] = '#'
+        map[6][4] = '#'
+        ss = successors(map, 4, 5, '#')
+        assert ss == [(3, 4), (3, 5), (4, 4),
+                      (5, 4), (5, 5), (5, 6)]
+
+    def test_find_path_astar(self):
+        map = [['x' for x in range(10)] for y in range(10)]
+        path = find_path_astar(map, 1, 2, 7, 6)
+        assert path == [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 6), (7, 6)]
+
+    def test_find_path_astar_obstacles(self):
+        map = [
+            '..........',
+            '..........',
+            '.....####.',
+            '........#.',
+            '........#.',
+            '.########.',
+            '.....#....',
+            '.....#....',
+            '.....#....',
+            '..........',
+            '..........',
+        ]
+        path = find_path_astar(map, 7, 3, 4, 7, '#')
+        assert path == [(7, 3), (6, 4), (5, 4), (4, 4), (3, 4), (2, 4), (1, 4),
+                        (0, 5), (1, 6), (2, 7), (3, 7), (4, 7)]
+
+    def test_find_path_astar_obstacles_2(self):
+        map = [
+            '..........',
+            '..........',
+            '.....####.',
+            '........#.',
+            '........#.',
+            '.########.',
+            '.....#....',
+            '.....#....',
+            '.....#....',
+            '..........',
+            '..........',
+        ]
+        path = find_path_astar(map, 9, 3, 0, 5, '#')
+        assert path == [(9, 3), (9, 2), (8, 1), (7, 1), (6, 1), (5, 1), (4, 2),
+                        (3, 3), (2, 4), (1, 4), (0, 5)]
+
+    def test_find_path_astar_same_place(self):
+        map = [
+            '..',
+            '..',
+        ]
+        path = find_path_astar(map, 1, 0, 1, 0, '#')
+        assert path == [(1, 0)]
+
+    def test_find_path_astar_no_path(self):
+        map = [
+            '.....',
+            '.###.',
+            '.#.#.',
+            '.###.',
+            '.....',
+        ]
+        path = find_path_astar(map, 2, 2, 4, 4, '#')
+        assert path is None
